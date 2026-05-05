c7c4f4e3e1213e59df624f4c8d56910dc28810df
MartinMinggu <artminggu@gmail.com>
2026-05-05 14:29:39 +0700

    Basic Konowledge Python: Variabel , input ,operator, percabangan, perulangan, function, latihan 1 dan 2

---
+++ Learn-0/basicknowledge.py
@@ -0,0 +1,51 @@
+print("Hello World")
+
+# basic 
+# nama = "Budi"
+# umur = 20
+# tinggi = 170.5
+# is_student = True
+
+# print(nama)
+# print(umur)
+
+# Input dati user
+# nama = input("Masukan nama: ")
+# print("Hallo ", nama)
+
+# operator
+# a = 10
+# b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# percabangan
+# umur = 18
+# if umur >= 18:
+#     print("dewasa")
+# else:
+#     print("anak anak")
+
+
+# perulangan
+# for i in range(5):
+#     print(i)
+
+# function
+# def sapa(nama):
+#     print("Hallo ",nama)
+# sapa("martin")
+
+
+# latihan 1 
+# nama = input("masukan nama anda :")
+# umur = input("masukan umur anda :")
+
+# print("Hallo ", nama,". umur kamu ",umur," tahun")
+
+a = int(input("angkan pertama: "))
+b = int(input("angkan kedua: "))
+
+print(" Hasil: ", a+b)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BelajarPython
+# BelajarPython

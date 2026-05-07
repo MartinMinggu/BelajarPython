805785109ac22ee5d487b57b1810560470066f39
MartinMinggu <artminggu@gmail.com>
2026-05-07 11:00:33 +0700

    Belajar db sqllite bawaan python, ORM sederhana dan models dan auto compile tampilan ke View /Admin

---
+++ learnDjango/notes/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    judul = models.CharField(max_length=100)
+    konten = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Description(models.Model):
+    deskripsi = models.CharField(max_length=100)
+    kategori = models.TextField()
+
+# Note.objects.create(
+#     judul="Belajar Django",
+#     kontent="Hari ini belajar model"
+# )
+
+
+# Description.objects.create(
+#     deskripsi="Belajar Django",
+#     kategori="kategori1"
+# )
+

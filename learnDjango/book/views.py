9d4e0844b42f9f2c98b8e50f41c40f317dfacc8c
MartinMinggu <artminggu@gmail.com>
2026-05-07 14:21:26 +0700

    berhasil menambhkan tempate ke app books

--- learnDjango/book/views.py
+++ learnDjango/book/views.py
@@ -1,10 +1,22 @@
+# Menampilkan View biasa 
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from .models import Book
+# # Create your views here.
+# def book_list(request):
+#     books   = Book.objects.all()
+#     text    = ""
+#     for book in books:
+#         text += book.title + "<br>"
+#     return HttpResponse(text)
+
+# Menampilkan View berupa Html 
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Book
 # Create your views here.
 def book_list(request):
     books   = Book.objects.all()
-    text    = ""
-    for book in books:
-        text += book.title + "<br>"
-    return HttpResponse(text)
+    context = {
+        'books' : books
+    }
+    return render(request, 'book_list.html', context)
